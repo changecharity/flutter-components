7270787e37dffe2d16707c8b6434790f62bcabf3
chezky <chezkyfel@gmail.com>
2020-07-28 20:07:58 -0700

    added example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
 ## [0.0.3] - Paintings, SubmitRow, and TextField all imported through change_charity_components.dart
+
+## [0.0.4] - added example
---
+++ example/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:changecharitycomponents/change_charity_components.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Change Components Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _textController;
+  FocusNode _focusNode;
+  String _errMsg = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChangeTextInput(
+        controller: _textController,
+        focusNode: _focusNode,
+        hintText: "Password",
+        prefixIcon: Icons.lock,
+        isPassword: true,
+        errMsg: _errMsg,
+        errFunc: (s) {
+          setState(() {
+            _errMsg = s;
+          });
+        },
+      ),
+    );
+  }
+}
